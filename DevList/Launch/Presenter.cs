f163c5bcf7ea9828ea772aac873d13aac709445a
zaka4kin <zaka4kin@mail.ru>
2024-02-05 18:09:35 +0700

    Ещё один возврат к началу кода

--- DevList/Launch/Presenter.cs
+++ DevList/Launch/Presenter.cs
@@ -1,27 +1,49 @@
-﻿using LaunchModelSpace;
+﻿using INIFileSpace;
+using System.Windows.Forms;
 using BaseFormViewSpace;
 
 namespace LaunchPresenterSpace
 {
     public class LaunchPresenter
     {
-        // Для работы с логикой создаём Model
-        LaunchModel launchModel = new LaunchModel();
+        // Файл с настройками
+        INIFile iniFile;
 
         // Обработка нажатия кнопки "Загрузить"
-        public void Download() { launchModel.Download(); BaseFormLoad(); }
+        public void Download()
+        {
+            iniFile = new INIFile();
+
+            BaseFormLoad();
+        }
 
         // Обработка нажатия кнопки "Создать"
-        public void Create() { launchModel.Create(); BaseFormLoad(); }
+        public void Create()
+        {
+            DialogResult result = MessageBox.Show("Данное действие очистит базу!", "Перезаписать файлы?", MessageBoxButtons.YesNo);
+
+            // Если перезапись одобрена, формируем новую, пустую БД и сопутствующие файлы и папки
+            if (result == DialogResult.Yes) { iniFile = new INIFile(Application.StartupPath); }
+
+            BaseFormLoad();
+        }
 
         // Обработка нажатия кнопки "Открыть"
-        public void Open() { launchModel.Open(); BaseFormLoad(); }
+        public void Open()
+        {
+            OpenFileDialog openFile = new() { Filter = "*.INI|*.ini" };
+
+            // Если файл существует, открываем БД
+            if (openFile.ShowDialog() == DialogResult.OK) { iniFile = new INIFile(openFile.FileName); }
+
+            BaseFormLoad();
+        }
 
         // Создаём основную форму для работы в БД
         public void BaseFormLoad()
         {
             // Передаём путь до файла с "настройками"
-            BaseFormView baseFormView = new BaseFormView("DevList 6.9 - Главное окно", launchModel.iniFile);
+            BaseFormView baseFormView = new("DevList 6.9 - Главное окно", iniFile);
 
             // Показываем форму
             baseFormView.ShowDialog();
