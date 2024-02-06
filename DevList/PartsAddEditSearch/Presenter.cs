0c06263d0b8672a06fd39e2431c5d7a2fefc177b
zaka4kin <zaka4kin@mail.ru>
2024-02-06 08:57:00 +0700

    Подготовка MVP для PartsAddEditSearch

--- DevList/PartsAddEditSearch/Presenter.cs
+++ DevList/PartsAddEditSearch/Presenter.cs
@@ -1,5 +1,8 @@
 ﻿using PartsAddEditSearchViewSpace;
 using PartsAddEditSearchModelSpace;
+using System.CodeDom.Compiler;
+using System;
+using INIFileSpace;
 
 namespace PartsAddEditSearchPresenterSpace
 {
@@ -7,11 +10,202 @@ namespace PartsAddEditSearchPresenterSpace
     {
         PartsAddEditSearchView partsAddEditSearchView;
 
-        public PartsAddEditSearchModel partsAddEditSearchModel = new();
+        PartsAddEditSearchModel partsAddEditSearchModel = new();
 
         public PartsAddEditSearchPresenter(PartsAddEditSearchView partsAddEditSearchView)
         {
             this.partsAddEditSearchView = partsAddEditSearchView;
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            Result[0] = string.Empty;
+            Result[1] = Number.Text;
+            Result[2] = Date.Text;
+            Result[3] = CPU.Text;
+            Result[4] = Mainboard.Text;
+            Result[5] = RAM.Text;
+            Result[6] = Disk.Text;
+            Result[7] = Videocard.Text;
+            Result[8] = Power.Text;
+            Result[9] = Case.Text;
+            Result[10] = Year.Text;
+            Result[11] = string.Empty;
+            Result[12] = string.Empty;
+
+            AddInEnd = addInEnd.Checked;
+
+            Execute = true;
+
+            Close();
+        }
+
+        private void ButtonCPUPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.CPUs);
+
+            list.Add(CPU.Text);
+
+            CPU.Items.Clear();
+
+            CPU.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonCPUMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.CPUs);
+
+            list.Remove(CPU.Text);
+
+            CPU.Items.Clear();
+
+            CPU.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonMainboardPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Mainboards);
+
+            list.Add(Mainboard.Text);
+
+            Mainboard.Items.Clear();
+
+            Mainboard.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonMainboardMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Mainboards);
+
+            list.Remove(Mainboard.Text);
+
+            Mainboard.Items.Clear();
+
+            Mainboard.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonRAMPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.RAMs);
+
+            list.Add(RAM.Text);
+
+            RAM.Items.Clear();
+
+            RAM.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonRAMMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.RAMs);
+
+            list.Remove(RAM.Text);
+
+            RAM.Items.Clear();
+
+            RAM.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonDiskPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Storges);
+
+            list.Add(Disk.Text);
+
+            Disk.Items.Clear();
+
+            Disk.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonDiskMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Storges);
+
+            list.Remove(Disk.Text);
+
+            Disk.Items.Clear();
+
+            Disk.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonVideocardPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Videocards);
+
+            list.Add(Videocard.Text);
+
+            Videocard.Items.Clear();
+
+            Videocard.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonVideocardMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Videocards);
+
+            list.Remove(Videocard.Text);
+
+            Videocard.Items.Clear();
+
+            Videocard.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonPowerPlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Powers);
+
+            list.Add(Power.Text);
+
+            Power.Items.Clear();
+
+            Power.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonPowerMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Powers);
+
+            list.Remove(Power.Text);
+
+            Power.Items.Clear();
+
+            Power.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonCasePlus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Cases);
+
+            list.Add(Case.Text);
+
+            Case.Items.Clear();
+
+            Case.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ButtonCaseMinus_Click(object sender, EventArgs e)
+        {
+            List list = new List(iniFile.Cases);
+
+            list.Remove(Case.Text);
+
+            Case.Items.Clear();
+
+            Case.Items.AddRange(File.ReadAllLines(list.Path));
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Number.Text =
+            Date.Text =
+            CPU.Text =
+            Mainboard.Text =
+            RAM.Text =
+            Disk.Text =
+            Videocard.Text =
+            Power.Text =
+            Case.Text =
+            Year.Text = string.Empty;
         }
     }
 }
--- DevList/PartsAddEditSearch/View.cs
+++ DevList/PartsAddEditSearch/View.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Windows.Forms;
-using System.IO;
 using AbstractAddEditSearchSpace;
 using INIFileSpace;
-using ListSpace;
 using PartsAddEditSearchPresenterSpace;
+using ListSpace;
+using System.IO;
 
 namespace PartsAddEditSearchViewSpace
 {
@@ -37,10 +36,7 @@ namespace PartsAddEditSearchViewSpace
                 Case.Text = str[9];
                 Year.Text = str[10];
             }
-        }
 
-        private void PartsSearchEditWindow_Load(object sender, EventArgs e)
-        {
             List cpu = new List(iniFile.CPUs);
             List mainboard = new List(iniFile.Mainboards);
             List ram = new List(iniFile.RAMs);
@@ -58,204 +54,38 @@ namespace PartsAddEditSearchViewSpace
             Case.Items.AddRange(File.ReadAllLines(cases.Path));
         }
 
-        private void Execute_Click(object sender, EventArgs e)
-        {
-            Result[0] = string.Empty;
-            Result[1] = Number.Text;
-            Result[2] = Date.Text;
-            Result[3] = CPU.Text;
-            Result[4] = Mainboard.Text;
-            Result[5] = RAM.Text;
-            Result[6] = Disk.Text;
-            Result[7] = Videocard.Text;
-            Result[8] = Power.Text;
-            Result[9] = Case.Text;
-            Result[10] = Year.Text;
-            Result[11] = string.Empty;
-            Result[12] = string.Empty;
+        private void Execute_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
 
-            AddInEnd = addInEnd.Checked;
+        private void ButtonCPUPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
 
-            Execute = true;
+        private void ButtonCPUMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
 
-            Close();
-        }
+        private void ButtonMainboardPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonMainboardMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonRAMPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonRAMMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonDiskPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonDiskMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonVideocardPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonVideocardMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonPowerPlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonPowerMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonCasePlus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ButtonCaseMinus_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
+
+        private void ClearButton_Click(object sender, EventArgs e) { partsAddEditSearchPresenter. }
 
         private void Close_Click(object sender, EventArgs e) { Close(); }
-
-        private void ButtonCPUPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.CPUs);
-
-            list.Add(CPU.Text);
-
-            CPU.Items.Clear();
-
-            CPU.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonCPUMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.CPUs);
-
-            list.Remove(CPU.Text);
-
-            CPU.Items.Clear();
-
-            CPU.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonMainboardPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Mainboards);
-
-            list.Add(Mainboard.Text);
-
-            Mainboard.Items.Clear();
-
-            Mainboard.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonMainboardMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Mainboards);
-
-            list.Remove(Mainboard.Text);
-
-            Mainboard.Items.Clear();
-
-            Mainboard.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonRAMPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.RAMs);
-
-            list.Add(RAM.Text);
-
-            RAM.Items.Clear();
-
-            RAM.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonRAMMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.RAMs);
-
-            list.Remove(RAM.Text);
-
-            RAM.Items.Clear();
-
-            RAM.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonDiskPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Storges);
-
-            list.Add(Disk.Text);
-
-            Disk.Items.Clear();
-
-            Disk.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonDiskMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Storges);
-
-            list.Remove(Disk.Text);
-
-            Disk.Items.Clear();
-
-            Disk.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonVideocardPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Videocards);
-
-            list.Add(Videocard.Text);
-
-            Videocard.Items.Clear();
-
-            Videocard.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonVideocardMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Videocards);
-
-            list.Remove(Videocard.Text);
-
-            Videocard.Items.Clear();
-
-            Videocard.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonPowerPlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Powers);
-
-            list.Add(Power.Text);
-
-            Power.Items.Clear();
-
-            Power.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonPowerMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Powers);
-
-            list.Remove(Power.Text);
-
-            Power.Items.Clear();
-
-            Power.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonCasePlus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Cases);
-
-            list.Add(Case.Text);
-
-            Case.Items.Clear();
-
-            Case.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void ButtonCaseMinus_Click(object sender, EventArgs e)
-        {
-            List list = new List(iniFile.Cases);
-
-            list.Remove(Case.Text);
-
-            Case.Items.Clear();
-
-            Case.Items.AddRange(File.ReadAllLines(list.Path));
-        }
-
-        private void PartsSearchEditWindow_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter) { Execute_Click(sender, e); }
-
-            if (e.KeyCode == Keys.Escape) { Close_Click(sender, e); }
-        }
-
-        private void ClearButton_Click(object sender, EventArgs e)
-        {
-            Number.Text =
-            Date.Text =
-            CPU.Text =
-            Mainboard.Text =
-            RAM.Text =
-            Disk.Text =
-            Videocard.Text =
-            Power.Text =
-            Case.Text =
-            Year.Text = string.Empty;
-        }
     }
 }
